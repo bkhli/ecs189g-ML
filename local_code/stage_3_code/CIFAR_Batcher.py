e29a8d998e80a27d75064270cd46547dac2b6b40
bkhli <194544471+bkhli@users.noreply.github.com>
2025-05-09 00:32:06 -0700

    run on cuda

--- local_code/stage_3_code/CIFAR_Batcher.py
+++ local_code/stage_3_code/CIFAR_Batcher.py
@@ -4,10 +4,12 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
 class CIFAR_Dataset(Dataset):
     def __init__(self, X, y):
-        self.X = torch.tensor(np.array(X), dtype=torch.float32) / 255.0  # normalize
-        self.y = torch.tensor(np.array(y), dtype=torch.long)
+        self.X = torch.tensor(np.array(X), dtype=torch.float32).to(device=device) / 255.0  # normalize
+        self.y = torch.tensor(np.array(y), dtype=torch.long).to(device=device)
 
     def __len__(self):
         return len(self.X)
