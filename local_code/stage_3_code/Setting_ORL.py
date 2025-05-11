262a7677e7906185a6cebf475202758bb90dd0d3
nivreddy <nivreddy@ucdavis.edu>
2025-05-11 00:33:54 -0700

    updating

--- local_code/stage_3_code/Setting_ORL.py
+++ local_code/stage_3_code/Setting_ORL.py
@@ -9,11 +9,15 @@ import numpy as np
 class Setting_Train_Test(setting):
     fold = 3
 
-    def prepare(self, sDataset, sMethod, sResult, sEvaluate):
+    def prepare(self, sDataset, sMethod, sResult, sEvaluateAcc, sEvaluateF1None, sEvaluateF1Macro, sEvaluateF1Micro, sEvaluateF1Weighted):
         self.dataset = sDataset
         self.method = sMethod
         self.result = sResult
-        self.evaluate = sEvaluate
+        self.evaluate = sEvaluateAcc
+        self.evaluate_f1_none = sEvaluateF1None
+        self.evaluate_f1_macro = sEvaluateF1Macro
+        self.evaluate_f1_micro = sEvaluateF1Micro
+        self.evaluate_f1_weighted = sEvaluateF1Weighted
     
     def load_run_save_evaluate(self):
         
@@ -61,6 +65,10 @@ class Setting_Train_Test(setting):
         self.result.save()
             
         self.evaluate.data = learned_result
+        self.evaluate_f1_none.data = learned_result
+        self.evaluate_f1_macro.data = learned_result
+        self.evaluate_f1_micro.data = learned_result
+        self.evaluate_f1_weighted.data = learned_result
         
-        return self.evaluate.evaluate(), None
+        return self.evaluate.evaluate(), self.evaluate_f1_none.evaluate(), self.evaluate_f1_macro.evaluate(), self.evaluate_f1_micro.evaluate(), self.evaluate_f1_weighted.evaluate()
 
