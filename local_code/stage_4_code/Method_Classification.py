504ba4713e7e15380d51e8ec39367bea269b4f2b
diegotyner <diego.tyner@gmail.com>
2025-05-20 20:42:41 -0700

    all methods have CUDA

--- local_code/stage_4_code/Method_Classification.py
+++ local_code/stage_4_code/Method_Classification.py
@@ -35,10 +35,10 @@ class Method_RNN(method, nn.Module):
 
         self.data = None
         # it defines the max rounds to train the model
-        self.max_epoch = 50
+        self.max_epoch = 150
         # it defines the learning rate for gradient descent based optimizer for model learning
-        self.learning_rate = 5e-5
-        self.batch_size = 256
+        self.learning_rate = 3e-5
+        self.batch_size = 2048
 
         assert vocab is not None, "[BUG] vocab is None when passed to Method_MLP"
         # print("[DEBUG] vocab type:", type(vocab))
@@ -47,7 +47,7 @@ class Method_RNN(method, nn.Module):
         self.vocab = vocab
         self.vocab_size = len(vocab)
         self.embedding_dim = 100
-        self.hidden_size = 128  # 20
+        self.hidden_size = 256  # 20
         self.num_layers = 2
         self.bidirectional = False
 
@@ -55,12 +55,15 @@ class Method_RNN(method, nn.Module):
         self.rnn = nn.RNN(
             self.embedding_dim, self.hidden_size, self.num_layers, batch_first=True
         )
-        self.dropout = nn.Dropout(0.3)
+        self.dropout = nn.Dropout(0.5)
         self.fc = nn.Linear(self.hidden_size * (2 if self.bidirectional else 1), 1)
+
+        self.to(device)
 
     # it defines the forward propagation function for input x
     # this function will calculate the output layer by layer
     def forward(self, x) -> torch.Tensor:
+        x = x.to(device)
         embeddings = self.embedding(x)
         outputs, hidden_out = self.rnn(embeddings)
         # Outputs: [batches, seq_len, hidden]
@@ -74,7 +77,9 @@ class Method_RNN(method, nn.Module):
     # backward error propagation will be implemented by pytorch automatically
     # so we don't need to define the error backpropagation function here
 
-    def train(self, X, y):
+    # I renamed this so we could have a test mode and train mode
+    # Previously conflicted with pytorch naming conventions
+    def train_renamed(self, X, y):
         # ic(len(y))
         # ic(len(X))
         # ic(X[0])
@@ -112,19 +117,24 @@ class Method_RNN(method, nn.Module):
         y_tensor = torch.tensor(np.array(y), dtype=torch.float32).to(device=device)
         train_dataset = TensorDataset(X_tensor, y_tensor)
         train_loader = DataLoader(
-            train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=4
+            train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=0
         )  # Could replace with num_workers=0,1,2,..
+        # For running on google colab, needs to be zero?
         loss_tracker = TrainLoss()
         for epoch in range(
             self.max_epoch
         ):  # you can do an early stop if self.max_epoch is too much...
             print(epoch)
+            self.train(True)
 
             for idx, (X, y_true) in enumerate(train_loader):
                 # ic(X.shape)
                 # ic(y_true.shape)
 
                 # get the output, we need to covert X into torch.tensor so pytorch algorithm can operate on it
+
+                X = X.to(device)
+                y_true = y_true.to(device)
                 y_pred = self.forward(X)
                 # ic(y_pred.shape)
 
@@ -162,6 +172,7 @@ class Method_RNN(method, nn.Module):
                     )
                 if epoch % 2 == 0 and idx == 0:
                     test_preds = self.test(self.data["test"]["X"])
+                    self.train()
                     accuracy_evaluator.data = {
                         "true_y": self.data["test"]["y"],
                         "pred_y": test_preds.detach().cpu().numpy(),
@@ -179,18 +190,18 @@ class Method_RNN(method, nn.Module):
 
         y_preds = []
 
-        # self.eval()
+        self.eval()
         with torch.no_grad():
             for (X,) in test_loader:
                 outputs = self.forward(X)
                 batch_preds = torch.round(torch.sigmoid(outputs))
-                y_preds.append(batch_preds)
-        return torch.cat(y_preds)
+                y_preds.append(batch_preds.cpu())
+        return torch.cat(y_preds).cpu()
 
     def run(self):
         print("method running...")
         print("--start training...")
-        self.train(self.data["train"]["X"], self.data["train"]["y"])
+        self.train_renamed(self.data["train"]["X"], self.data["train"]["y"])
         print("--start testing...")
         pred_y = self.test(self.data["test"]["X"])
         return {"pred_y": pred_y, "true_y": self.data["test"]["y"]}
