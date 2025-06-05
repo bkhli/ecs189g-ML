dd2287517d902ee499fd5743190571725ffa5fe9
Nivita Reddy <nivitareddy@campus-019-021.ucdavis.edu>
2025-06-04 17:55:24 -0700

    added other scripts, TODO: improve accuracy, clean up code

--- local_code/stage_5_code/Dataset_Loader_Node_Classification.py
+++ local_code/stage_5_code/Dataset_Loader_Node_Classification.py
@@ -49,39 +49,9 @@ class Dataset_Loader(dataset):
 
         # load node data from file
         idx_features_labels = np.genfromtxt("{}/node".format(self.dataset_source_folder_path), dtype=np.dtype(str))
-        print("PRINTING SIZE")
-        print(idx_features_labels)
         features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
         onehot_labels = self.encode_onehot(idx_features_labels[:, -1]) # this should be the y
-        print(onehot_labels)
 
-        idx_feature_mapping = {(1, 0, 0, 0, 0, 0, 0) : [],
-                               (0, 1, 0, 0, 0, 0, 0) : [],
-                               (0, 0, 1, 0, 0, 0, 0) : [],
-                               (0, 0, 0, 1, 0, 0, 0) : [],
-                               (0, 0, 0, 0, 1, 0, 0) : [],
-                               (0, 0, 0, 0, 0, 1, 0) : [],
-                               (0, 0, 0, 0, 0, 0, 1) : []}
-        
-
-
-#         for input_item, one_hot_label in data:
-#             output_dict[output_label].append(input_item)
-
-#         # Convert to regular dict if needed
-# o       utput_dict = dict(output_dict)
-        print("Should be printing indices of nodes")
-        print(idx_features_labels[:, 0])
-
-        print("Should be printing length of features")
-        print(features.shape[0])
-
-
-        for i in range(features.shape[0]):
-            idx_feature_mapping[tuple(onehot_labels[i])].append(i)
-
-        print("Printing feature mapping")
-        print(idx_feature_mapping)
 
 
         # load link data from file and build graph
@@ -99,24 +69,66 @@ class Dataset_Loader(dataset):
         labels = torch.LongTensor(np.where(onehot_labels)[1])
         adj = self.sparse_mx_to_torch_sparse_tensor(norm_adj)
 
-        # Still need to randomly sample these, also account for when there's different cases
         idx_train = []
         idx_test = []
-        np.random.seed(2)
 
-        for key in idx_feature_mapping:
-            # print("printing i")
-            # print(i)
-           # sampled_set = np.random.choice(idx_feature_mapping[i], size=20, replace=False)
-            sampled_set = set(random.sample(idx_feature_mapping[key], 20))
-            complement = set(idx_feature_mapping[key]) - sampled_set
-            idx_train+=sampled_set
-            idx_test+=complement
+        # same as what was in the other file
+        np.random.seed(2) 
 
-        # idx_train = range(140)
-        # idx_test = range(140, 140+1050)
+
+        # the following part, you can either put them into the setting class or you can leave them in the dataset loader
+        # the following train, test, val index are just examples, sample the train, test according to project requirements
+        if self.dataset_name == 'cora':
+            idx_feature_mapping = {(1, 0, 0, 0, 0, 0, 0) : [],
+                                (0, 1, 0, 0, 0, 0, 0) : [],
+                                (0, 0, 1, 0, 0, 0, 0) : [],
+                                (0, 0, 0, 1, 0, 0, 0) : [],
+                                (0, 0, 0, 0, 1, 0, 0) : [],
+                                (0, 0, 0, 0, 0, 1, 0) : [],
+                                (0, 0, 0, 0, 0, 0, 1) : []}
+            for i in range(features.shape[0]):
+                idx_feature_mapping[tuple(onehot_labels[i])].append(i)
+            for key in idx_feature_mapping:
+                sampled_set = set(random.sample(idx_feature_mapping[key], 20))
+                complement = set(idx_feature_mapping[key]) - sampled_set
+                idx_train.extend(sampled_set)
+                idx_test.extend(complement)
+            
+        elif self.dataset_name == 'citeseer':
+            idx_feature_mapping = {(1, 0, 0, 0, 0, 0) : [],
+                                (0, 1, 0, 0, 0, 0) : [],
+                                (0, 0, 1, 0, 0, 0) : [],
+                                (0, 0, 0, 1, 0, 0) : [],
+                                (0, 0, 0, 0, 1, 0) : [],
+                                (0, 0, 0, 0, 0, 1) : []}
+            for i in range(features.shape[0]):
+                idx_feature_mapping[tuple(onehot_labels[i])].append(i)
+            for key in idx_feature_mapping:
+                sampled_set = set(random.sample(idx_feature_mapping[key], 20))
+                complement = set(idx_feature_mapping[key]) - sampled_set
+                idx_train.extend(sampled_set)
+                idx_test.extend(complement)
+        elif self.dataset_name == 'pubmed':
+            idx_feature_mapping = {(1, 0, 0) : [],
+                                (0, 1, 0) : [],
+                                (0, 0, 1) : []}
+            for i in range(features.shape[0]):
+                idx_feature_mapping[tuple(onehot_labels[i])].append(i)
+            for key in idx_feature_mapping:
+                sampled_set = set(random.sample(idx_feature_mapping[key], 20))
+                complement = set(idx_feature_mapping[key]) - sampled_set
+                idx_train.extend(sampled_set)
+                idx_test.extend(complement)
+        #---- cora-small is a toy dataset I hand crafted for debugging purposes ---
+        elif self.dataset_name == 'cora-small':
+            idx_train = range(5)
+            idx_val = range(5, 10)
+            idx_test = range(5, 10)
+
+        print("Printing idx train")
+        print(idx_train)
+
         idx_train = torch.LongTensor(idx_train)
-        #idx_val = torch.LongTensor(idx_val)
         idx_test = torch.LongTensor(idx_test)
 
 
---
+++ script/stage_5_script/script_citeseer.py
@@ -0,0 +1,54 @@
+from local_code.stage_5_code.Dataset_Loader_Node_Classification import Dataset_Loader
+from local_code.stage_5_code.Method_MLP import GCN
+from local_code.stage_5_code.Result_Saver import Result_Saver
+from local_code.stage_5_code.Setting import Setting_Train_Test
+from local_code.stage_3_code.Evaluate_Accuracy import Evaluate_Accuracy
+
+
+
+import numpy as np
+import torch
+import torch.onnx
+
+#---- Multi-Layer Perceptron script ----
+if 1:
+    #---- parameter section -------------------------------
+    np.random.seed(2)
+    torch.manual_seed(2)
+    #------------------------------------------------------
+
+    # ---- objection initialization setction ---------------
+    data_obj = Dataset_Loader('', '')
+    # Note - might need to change paths, but it seems like it should be this?
+    data_obj.dataset_source_folder_path = 'data/stage_5_data/citeseer'
+    data_obj.dataset_name = 'citeseer'
+
+    # Come back to this
+    # Oh, this is how it passes into the method function
+    method_obj = GCN(nfeat=3703,
+            nhid=16,
+            nclass=6,
+            dropout=0.5)
+
+    # probably don't need to change anything?
+    result_obj = Result_Saver('saver', '')
+    result_obj.result_destination_folder_path = 'result/stage_5_result/MLP_'
+    result_obj.result_destination_file_name = 'prediction_result'
+
+    setting_obj = Setting_Train_Test('train and test', '')
+
+    evaluate_obj = Evaluate_Accuracy('accuracy', '')
+    # ------------------------------------------------------
+
+    # ---- running section ---------------------------------
+    print('************ Start ************')
+    setting_obj.prepare(data_obj, method_obj, result_obj, evaluate_obj)
+    setting_obj.print_setup_summary()
+    mean_score, std_score = setting_obj.load_run_save_evaluate()
+    print('************ Overall Performance ************')
+    print('MLP Accuracy: ' + str(mean_score) + ' +/- ' + str(std_score))
+    print('************ Finish ************')
+    # ------------------------------------------------------
+    
+
+    
